c133ccce9a7056442f812ec2e04459a40e031209
Tim <wvz241@haw-hamburg.de>
2025-06-12 15:31:54 +0200

    simplified tests (DONT MERGE IN MAIN)

--- src/app.py
+++ src/app.py
@@ -7,7 +7,6 @@ from move_handlers.mcts_handler import MCTSHandler # type: ignore
 from move_handlers.console_handler import ConsoleHandler  # type: ignore
 from move_handlers.minimax_handler import MinimaxHandler  # type: ignore
 from move_handlers.minimax_iterative_handler import MinimaxIterativeHandler  # type: ignore
-from move_handlers.move_handler import MoveHandler
 from move_handlers.random_handler import RandomHandler  # type: ignore
 from uttt_board import BoardIndex, UTTTBoard
 from tqdm import tqdm
@@ -39,32 +38,7 @@ def modify_board(board: UTTTBoard) -> None:
 def main():
     print("Running...")
 
-    file_name = "boards_dataset.csv"
-    
-
-
-    #with open("model_comparison.csv", "a") as f:
-    #    f.write(f"Model 1;Model 2;Wins X;Wins Y;Draws\n")
-
-    # Random vs Minimax
-    #compare_models(RandomHandler("X", False), MinimaxIterativeHandler("O", 0.1, False), 1000)
-    #compare_models(MinimaxIterativeHandler("X", 0.1, False), RandomHandler("O", False), 1000)
-    #
-    ## Random vs MCTS
-    #compare_models(RandomHandler("X", False), MCTSHandler("O", 0.1, False), 1000)
-    #compare_models(MCTSHandler("X", 0.1, False), RandomHandler("O", False), 1000)
-
-    # MCTS vs Minimax
-    # compare_models(MCTSHandler("X", 0.1, False), MinimaxIterativeHandler("O", 0.1, False), 1000)
-    # compare_models(MinimaxIterativeHandler("X", 0.1, False), MCTSHandler("O", 0.1, False), 1000)
-    # print("All comparisons done")
-
-    # MCTS moves with increasing time limits
-    # compare_runtime([5])
-
-    # compare_models(MinimaxIterativeHandler("X", 0.1, False), MinimaxIterativeHandler("O", 0.1, False), 1000)
-
-    # analyse_branching_factor()
+    create_dataset(10_000, "boards_dataset.csv")
 
     print("All done")
 
@@ -76,7 +50,7 @@ def create_dataset(board_cound: int, file_name: str):
 
     progress_bar = tqdm(
         range(board_cound),
-        desc="Creating dataset",
+        desc=f"Creating dataset in file {file_name}",
         bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}'
     )
 
@@ -87,115 +61,3 @@ def create_dataset(board_cound: int, file_name: str):
         
         state = GameState(player_dumb, player_smart, last_move_naive, index=i, file_name=file_name)
         state.run_game_praktikum() # automatically creates the entries for one board
-
-
-def compare_runtime(time_limits: List[float]):
-    # with open("runtime_comparison.csv", "w") as f:
-    #     f.write(f"Model 1;Model 2;Runtime 1; Runtime 2;Wins X;Wins Y;Draws;Max-Value 1;Max-Value 2\n")
-        
-    for i in time_limits:
-        model1 = MCTSHandler
-        model2 = MinimaxIterativeHandler
-        run_comparison(model1, model2, i) # increase time limit for MCTS
-        run_comparison(model2, model1, i) # increase time limit for Minimax
-            
-def run_comparison(model1: type[MCTSHandler] | type[MinimaxIterativeHandler], model2: type[MCTSHandler] | type[MinimaxIterativeHandler], time: float):
-    x_wins = 0
-    o_wins = 0
-    draws = 0
-    progress_bar = tqdm(
-        range(50),
-        desc=f"{model1.__name__}: {time} seconds vs {model2.__name__}: 0.1 seconds",
-        bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}'
-    )
-    algo1 = None
-    algo2 = None
-    for _ in progress_bar:
-        try:
-            algo1 = model1("X", time, False)
-            algo2 = model2("O", 0.1, False)
-            board = UTTTBoard()
-            state = GameState(board, algo1, algo2)
-            state.run_game_loop(False)
-
-            if board.winner == "X": 
-                x_wins += 1
-            elif board.winner == "O":
-                o_wins += 1
-            else:
-                draws += 1
-        except KeyboardInterrupt:
-            print("\nGame interrupted. Exiting...")
-            return
-        
-    with open("runtime_comparison.csv", "a") as f:
-        f.write(f"{model1.__name__};{model2.__name__};{time};0.1;{x_wins};{o_wins};{draws};{algo1.get_max_value() if algo1 else 0};{algo2.get_max_value() if algo2 else 0}\n")
-
-def compare_models(playerX: MoveHandler, playerO: MoveHandler, games: int = 100):
-    x_wins = 0
-    o_wins = 0
-    draws = 0
-
-    progress_bar = tqdm(
-        range(games),
-        desc=f"{playerX.__class__.__name__} vs {playerO.__class__.__name__}",
-        bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}'
-    )
-
-    for _ in progress_bar:
-        board = UTTTBoard()
-        state = GameState(board, playerX, playerO)
-
-        try:
-            state.run_game_loop(False)
-        except KeyboardInterrupt:
-            print("\nGame interrupted. Exiting...")
-            break
-
-        if board.winner == "X":
-            x_wins += 1
-        elif board.winner == "O":
-            o_wins += 1
-        else:
-            draws += 1
-
-    with open("model_comparison.csv", "a") as f:
-        f.write(f"{playerX.__name__};{playerO.__name__};{x_wins};{o_wins};{draws}\n")
-    
-def analyse_branching_factor():
-    with open("branching_factor.csv", "w") as f:
-        f.write("Move;Possible Moves Total;Simulation Count\n")
-
-    branching_coll = dict[int, tuple[int, int]]()
-
-    progress_bar = tqdm(
-        range(300),
-        desc="Branching Factor Analysis",
-        bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}'
-    )
-    
-    for _ in progress_bar:
-        handler1 = MCTSHandler("X", 0.1, False)
-        handler2 = MCTSHandler("O", 0.1, False)
-        board = UTTTBoard()
-        state = GameState(board, handler1, handler2)
-        state.run_game_loop(False)
-
-        for move, count in handler1.branching_factor.items():
-            if move not in branching_coll:
-                branching_coll[move] = count, 1
-            else:
-                branching_coll[move] = count + branching_coll[move][0], 1 + branching_coll[move][1]
-        
-        for move, count in handler2.branching_factor.items():
-            if move not in branching_coll:
-                branching_coll[move] = count, 1
-            else:
-                branching_coll[move] = count + branching_coll[move][0], 1 + branching_coll[move][1]
-
-    with open("branching_factor.csv", "a") as f:
-        for move, count in branching_coll.items():
-            f.write(f"{move};{count[0]};{count[1]}\n")
-
-if __name__ == "__main__":
-    main()
--- src/game_state.py
+++ src/game_state.py
@@ -8,7 +8,7 @@ from uttt_board import BoardIndex, UTTTBoard
 class GameState:
     """Class to manage the game state."""
 
-    def __init__(self, player_dumb: RandomHandler, player_smart: MinimaxHandler, last_move_naive : int, index: int = 0, file_name: str):
+    def __init__(self, player_dumb: RandomHandler, player_smart: MinimaxHandler, last_move_naive : int, index: int, file_name: str):
         """
         Randomly simulates a game until last_move_naive is reached, upon which it switches to a smart player.
         Gamestates after last_move_naive are written to file.
@@ -150,68 +150,3 @@ class GameState:
         with open(self.file_name, "a") as f:
             for board in self.boards_for_writing:
                 f.write(f"{self.index};{board.to_csv_row()};{self.current_forced_board_index};{self.board.winner}\n") # stimmt der forced-board-index hier?
-        
-
-    def run_game_loop(self, log: bool = True) -> None:
-        """Main game loop."""
-
-        while not self.game_over:
-            if log:
-                print("/\\" * 40)
-
-                self.board.display_board(self.current_forced_board_index)
-
-            board_index, cell_index = (
-                self.playerX.get_move(self.board, self.current_forced_board_index)
-                if self.current_player == "X"
-                else self.playerO.get_move(self.board, self.current_forced_board_index)
-            )
-
-            assert 0 <= board_index <= 8, "Board index must be between 0 and 8."
-            assert 0 <= cell_index <= 8, "Cell index must be between 0 and 8."
-
-            if (
-                self.current_forced_board_index is not None
-                and board_index != self.current_forced_board_index
-            ):
-                if log: print(
-                    f"Invalid move! You must play in board {self.current_forced_board_index}."
-                )
-                continue
-
-            current_small_board = self.board.get_small_board(board_index)
-            if current_small_board.winner is not None:
-                if current_small_board.winner == "Draw":
-                    if log: print(f"Board {board_index} is a draw! Try again.")
-                else:
-                    if log: print(
-                        f"Board {board_index} already won by {current_small_board.winner}! Try again."
-                    )
-                continue
-
-            if current_small_board.get_cell_value(cell_index) is not None:  # type: ignore
-                if log: print("Cell already occupied! Try again.")
-                continue
-
-            self.board.make_move(board_index, cell_index, self.current_player)  # type: ignore
-
-            if self.board.winner is not None:
-                self.game_over = True
-                if self.board.winner == "Draw":
-                    if log: print("Game over! It's a draw!")
-                else:
-                    if log: print(f"Game over! {self.board.winner} wins!")
-                if log: self.board.display_board(None)
-                break
-
-            # Allow the next move to be played only in the small board
-            # corresponding to the cell index of the last move
-            next_small_board = self.board.get_small_board(cell_index)
-            if next_small_board.winner is None:
-                self.current_forced_board_index = cell_index
-            else:
-                # If the next small board is already won, we can play anywhere
-                # Set current_board_index to None to allow any board
-                self.current_forced_board_index = None
-
-            self.__switch_player()
